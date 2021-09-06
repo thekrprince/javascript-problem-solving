f891050b52d9188a668f29d19a9a40474f393564
princekr96 <princekr700@gmail.com>
2021-09-06 17:27:39 +0530

    selection sort

---
+++ selection_sort.js
@@ -0,0 +1,25 @@
+// Selection sort
+
+function selectionSort(arr) {
+  var minIndex;
+
+  for (let i = 0; i < arr.length; i++) {
+    minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      var temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr;
+}
+
+var sortedArr = selectionSort([67, 12, 55, 32, 29, 100, 64]);
+console.log(sortedArr);
