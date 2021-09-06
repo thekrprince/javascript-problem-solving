8e49fe5bf43fe8c49e6b21ad6929e8843f148eb9
princekr96 <princekr700@gmail.com>
2021-09-06 23:55:38 +0530

    modified comment

--- merge_sort.js
+++ merge_sort.js
@@ -1,23 +1,24 @@
 /******************************
  *
- * Merge Sort
+ * MERGE SORT
  * It's a combination of two things - merging and sorting
  * Exploits the fact that arrays of 0 or 1 element are always sorted
  * Works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array
  * Use Divide & Conquer method
  *
- * 1. In order to implement merge sort, it's useful to 1st implement a function responsible for merging two sorted arrays.
- * 2. Given 2 arrays which are sorted, this helper function should create a new array which is also sorted, and consists of all of the elements in the 2 input arrays.
- * 3. The function should run in O(n+m) time and O(n+m) space and should not modify the parameters passed to it.
+ * HELPER
+ * => In order to implement merge sort, it's useful to 1st implement a function responsible for merging two sorted arrays.
+ * => Given 2 arrays which are sorted, this helper function should create a new array which is also sorted, and consists of all of the elements in the 2 input arrays.
+ * => The function should run in O(n+m) time and O(n+m) space and should not modify the parameters passed to it.
  *
- * Pseudocode - Merge
+ * PSEUDOCODE - Merge
  * 1. Create an empty array, take a look at the smallest values in each input array.
  * 2. While there are still values we haven't looked at...
  *      -> If the value in the 1st array is smaller than the value in the 2nd array, push the value in the 1st array into our results and move on to the next value in the 1st array.
  *      -> If the value in the 1st array is larger than the value in the 2nd array, push the value in the 2nd array into our results and move on to the next value in the 2nd array.
  *      -> Once we exhaust one array, push in all remaining values from the other array.
  *
- * mergeSort Pseudocode
+ * MERGESORT PSEUDOCODE
  * 1. Break up the array into halves until you have arrays that are empty or have one element.
  * 2. Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array.
  * 3. Once the arrays has been merged back together, return the merged and sorted array.
