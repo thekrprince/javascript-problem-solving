233ef1a479ea04481c7dcdf129612c39b09dd21e
princekr96 <princekr700@gmail.com>
2021-09-06 19:45:54 +0530

    Insertion sort code added

---
+++ insertion_sort.js
@@ -0,0 +1,16 @@
+// Insertion Sort
+
+function insertionSort(arr) {
+  for (var i = 1; i < arr.length; i++) {
+    var currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+
+  return arr;
+}
+
+var sortedArr = insertionSort([3, 1, 4, 11, 7]);
+console.log(sortedArr);
