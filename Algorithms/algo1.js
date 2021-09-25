f5b0efb637f27491bae7b4683f7c1c70d702c145
princekr96 <princekr700@gmail.com>
2021-09-26 02:00:43 +0530

    sudo code for 2 algo

---
+++ Algorithms/algo1.js
@@ -0,0 +1,15 @@
+// ===============================================================
+// Write a function which takes two numbers and returns their sum.
+// ===============================================================
+
+// 1. Can I restate the problem in my own words?
+'implement addition';
+// 2. What are the inputs that go into the problem?
+// - ints?
+// - floats?
+// - what about string for large numbers?
+// 3. What are the outputs that should come from the solution to the problrm?
+// - int? float? string?
+// 4. Can the outputs be determined from the input? In other words, do i have enough information to solve the problem?
+
+// 5. How should I label the important pieces of data that are a part of the problem?
---
+++ Algorithms/algo2.js
@@ -0,0 +1,17 @@
+// Write a function which takes in a string and returns counts of each character in the string.
+// "hello" -> o/p {h:1, e:1, l:2, o:1}
+// "Your PIN number is 1234!" -> o/p {1:1, 2:1, 3:1, 4:1, b:1, e:1, i:2, m:1, n:2, o:1, p:1, r:2, s:1, u:2, y:1}
+
+function charCount(str) {
+  // do something
+  // return an object with keys that are lowercase alphanumeric characters in the string; values should be the count for those characters
+}
+
+function charCount(str) {
+  // make object to return at end
+  // loop over string, for each character
+  // if the char is a number/letter AND is a key in object, add one to count
+  // if the char is a number/letter AND not in object, add it to object and set value to 1
+  // if char is something else (space, period, etc.) don't do anything
+  // return object at end
+}
