bb8c7df9c184321a4fd4795322fbc3388938bc05
princekr96 <princekr700@gmail.com>
2022-01-16 22:11:35 +0530

    added description in sumZero file

--- Algorithms/sumZero.js
+++ Algorithms/sumZero.js
@@ -1,5 +1,9 @@
 // Find the sum which gives the zero in result.
 
+/* Ques - Write a function called sumZero which accepts a sorted array of integers. The function
+should find the first pair where the sum is 0. Return an array that includes both values that sum
+to zero or undefined if a pair does not exist. */
+
 function sumZero(arr) {
   let left = 0;
   let right = arr.length - 1;
