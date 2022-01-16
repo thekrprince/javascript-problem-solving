b02cd6017a3ba148f6ae5ad0d9e9c948ccba6b5f
princekr96 <princekr700@gmail.com>
2022-01-16 07:55:05 +0530

    frequency counter anagram colt solution

---
+++ Algorithms/frequencyCounterAnagram2.js
@@ -0,0 +1,27 @@
+// Colt solution for Anagram frequency counter
+
+function validAnagram(first, second) {
+  if (first.length !== second.length) return false;
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? lookup[letter]++ : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+var res = validAnagram('aab', 'aba');
+console.log(res);
