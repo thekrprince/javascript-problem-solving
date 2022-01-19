ef91d7d380fccb9021d2a78d86be1d1d521843a7
princekr96 <princekr700@gmail.com>
2022-01-19 09:35:04 +0530

    solved areThereDuplicates problem

---
+++ Problem-Solving-Patterns/optional-challenges/areThereDuplicates.js
@@ -0,0 +1,31 @@
+/*
+  Implement a function called, areThereDuplicates which accepts a variable number of arguments, and
+  checks whether there are any duplicates among the arguments passed in.
+*/
+
+// Solution using Frequency Counter Pattern
+
+function areThereDuplicates(...args) {
+  let frequencyCounter = {};
+
+  for (let val of args) {
+    if (frequencyCounter[val]) {
+      frequencyCounter[val]++;
+    } else {
+      frequencyCounter[val] = 1;
+    }
+  }
+
+  console.log(frequencyCounter);
+
+  for (let key in frequencyCounter) {
+    if (frequencyCounter[key] === 2) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+var res = areThereDuplicates('a', 'b', 1, 5, 1);
+console.log(res);
