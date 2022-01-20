11f31d2509fa86f6d34cc476051f4db3c6bc0d18
princekr96 <princekr700@gmail.com>
2022-01-20 16:48:36 +0530

    solved maxSubArraySum

---
+++ Problem-Solving-Patterns/optional-challenges/maxSubArraySum.js
@@ -0,0 +1,28 @@
+/*
+  Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum
+  sum of a subarray with the length of the number passed to the function.
+*/
+
+function maxSubarraySum(arr, num) {
+  if (arr.length - 1 < num) return null;
+
+  var maxSum = 0;
+  var tempSum = 0;
+
+  for (let i = 0; i < num; i++) {
+    tempSum += arr[i];
+  }
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum + arr[i] - arr[i - num];
+    if (tempSum > maxSum) {
+      maxSum = tempSum;
+    }
+  }
+
+  return maxSum;
+}
+
+var arrVal = [100, 200, 300, 400];
+var res = maxSubarraySum(arrVal, 4);
+console.log(`maxSum is ${res}`);
