f5825b33c7a7d80e32f342a28a3935f52c12c1cb
princekr96 <princekr700@gmail.com>
2022-01-22 15:56:14 +0530

    collectOddValues

---
+++ Recursion/collectOddValues.js
@@ -0,0 +1,20 @@
+function collectOddValues(arr) {
+  let oddValues = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) return;
+
+    if (helperInput[0] % 2 !== 0) {
+      oddValues.push(helperInput[0]);
+    }
+
+    helper(helperInput.slice(1));
+  }
+
+  helper(arr);
+
+  return oddValues;
+}
+
+var res = collectOddValues([3, 2, 33, 7, 99, 43, 12, 8]);
+console.log(res);
