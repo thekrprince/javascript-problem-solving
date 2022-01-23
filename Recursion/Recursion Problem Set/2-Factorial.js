c86c50a9212f0e983c516e211ded93c2d4a62ab1
princekr96 <princekr700@gmail.com>
2022-01-23 08:34:10 +0530

    factorial

--- Recursion/Recursion Problem Set/2-Factorial.js
+++ Recursion/Recursion Problem Set/2-Factorial.js
@@ -1,12 +1,12 @@
 function factorial(num) {
   let totalVal = 1;
 
-  if (num === 1) return 1;
+  if (num === 1 || num === 0) return 1;
 
   totalVal = num * factorial(num - 1);
 
   return totalVal;
 }
 
-var res = factorial(5);
+var res = factorial(0);
 console.log(res);
