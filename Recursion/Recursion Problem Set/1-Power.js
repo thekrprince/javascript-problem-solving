bc9a10b09341781321601140cea7d7f8a224fc6f
princekr96 <princekr700@gmail.com>
2022-01-23 12:07:37 +0530

    power

---
+++ Recursion/Recursion Problem Set/1-Power.js
@@ -0,0 +1,12 @@
+/*
+  Write a function called power which accepts a base and an exponent. The function should return the power
+  of the base to the exponent. This function should mimic the functionality of Math.pow()
+*/
+
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
+
+var res = power(3, 4);
+console.log(res);
