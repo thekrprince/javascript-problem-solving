2786e69e0875157982ac0352cac7101010720744
princekr96 <princekr700@gmail.com>
2023-09-19 22:40:38 +0530

    multiple pointers problem 1

---
+++ Algorithm & Data Structure Theory/4.2-multiple-pointers1.js
@@ -0,0 +1,34 @@
+/*
+Write a function called sum2zero which accepts a sorted array of integers. The function should find the first pair of where the sum is 0.
+Return an array that included both values that sum to zero or undefined if a pair does not exist.
+*/
+
+function sum2zero(sortedArray) {
+  if (sortedArray.length <= 1) return false;
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  while (left < right) {
+    const sum = sortedArray[left] + sortedArray[right];
+    if (sum === 0) {
+      return [sortedArray[left], sortedArray[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return undefined;
+}
+
+const result = sum2zero([-3, -2, -1, 0, 1, 2, 3]);
+console.log(result);
+const result1 = sum2zero([-2, 0, 1, 3]);
+console.log(result1);
+const result2 = sum2zero([1, 2, 3]);
+console.log(result2);
+const result3 = sum2zero([1]);
+console.log(result3);
+const result4 = sum2zero([-3, -2, -1, 0, 2, 4, 6]);
+console.log(result4);
