76a04f3f949ab0a36f1df333e4624f29421b9e5e
princekr96 <princekr700@gmail.com>
2023-10-11 23:02:11 +0530

    optimized the soln

--- Algorithm & Data Structure Theory/4.2-multiple-pointers1.js
+++ Algorithm & Data Structure Theory/4.2-multiple-pointers1.js
@@ -3,32 +3,32 @@ Write a function called sum2zero which accepts a sorted array of integers. The f
 Return an array that included both values that sum to zero or undefined if a pair does not exist.
 */
 
-function sum2zero(sortedArray) {
+function sum2zero(sortedArray, target) {
   if (sortedArray.length <= 1) return false;
   let left = 0;
   let right = sortedArray.length - 1;
 
   while (left < right) {
     const sum = sortedArray[left] + sortedArray[right];
-    if (sum === 0) {
+    if (sum === target) {
       return [sortedArray[left], sortedArray[right]];
-    } else if (sum > 0) {
+    } else if (sum > target) {
       right--;
     } else {
       left++;
     }
   }
 
-  return undefined;
+  return "Not found";
 }
 
-const result = sum2zero([-3, -2, -1, 0, 1, 2, 3]);
+const result = sum2zero([-3, -2, -1, 0, 1, 2, 3], 0);
 console.log(result);
-const result1 = sum2zero([-2, 0, 1, 3]);
+const result1 = sum2zero([-2, 0, 1, 3], 1);
 console.log(result1);
-const result2 = sum2zero([1, 2, 3]);
+const result2 = sum2zero([1, 2, 3], 0);
 console.log(result2);
-const result3 = sum2zero([1]);
+const result3 = sum2zero([1], 0);
 console.log(result3);
-const result4 = sum2zero([-3, -2, -1, 0, 2, 4, 6]);
+const result4 = sum2zero([-3, -2, -1, 0, 2, 4, 6], 0);
 console.log(result4);
