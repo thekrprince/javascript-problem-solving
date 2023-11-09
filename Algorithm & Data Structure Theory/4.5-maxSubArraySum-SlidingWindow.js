c320c0dfc028f75c0db8d7eb5d7a71a4ad60a0ee
princekr96 <princekr700@gmail.com>
2023-11-09 21:48:53 +0530

    slidingWindow-prog

---
+++ Algorithm & Data Structure Theory/4.5-maxSubArraySum-SlidingWindow.js
@@ -0,0 +1,10 @@
+/**
+ * Write a function called maxSubArraySum which accepts an array of integers and a number called n.
+ * The function should calculate the maximum sum of n consecutive elements in the array.
+ **/
+
+function maxSubArraySum(arr, target) {
+  if (arr.length === 0) return false;
+}
+
+const res = maxSubArraySum([2, 5, 5, 8, 2, 1, 9, 12], 10);
