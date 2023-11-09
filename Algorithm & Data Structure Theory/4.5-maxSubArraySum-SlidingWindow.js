784f74fa7eaab233fceaefb28f9460ee6a84fd7b
princekr96 <princekr700@gmail.com>
2023-11-09 22:16:06 +0530

    maxSubArraySum

--- Algorithm & Data Structure Theory/4.5-maxSubArraySum-SlidingWindow.js
+++ Algorithm & Data Structure Theory/4.5-maxSubArraySum-SlidingWindow.js
@@ -3,8 +3,24 @@
  * The function should calculate the maximum sum of n consecutive elements in the array.
  **/
 
-function maxSubArraySum(arr, target) {
-  if (arr.length === 0) return false;
+function maxSubArraySum(arr, num) {
+  if (num > arr.length) return false;
+
+  var max = 0;
+
+  //   1st loop will run till "array.length - 1 + 1" so that it adds the required number.
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0;
+    // This loop will run to add the numbers and store it in the temp value.
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
 }
 
-const res = maxSubArraySum([2, 5, 5, 8, 2, 1, 9, 12], 10);
+const res = maxSubArraySum([2, 5, 5, 8, 2, 1, 9, 12], 3);
+console.log(res);
