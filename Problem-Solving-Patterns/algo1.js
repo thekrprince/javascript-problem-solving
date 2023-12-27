f7805a94b2422e6ed91f8fecb8b66f5f06159154
princekr96 <princekr700@gmail.com>
2023-12-27 20:01:26 +0530

    updated

--- Problem-Solving-Patterns/algo1.js
+++ Problem-Solving-Patterns/algo1.js
@@ -3,12 +3,12 @@
 // ===============================================================
 
 // 1. Can I restate the problem in my own words?
-'implement addition';
+"implement addition";
 // 2. What are the inputs that go into the problem?
 // - ints?
 // - floats?
 // - what about string for large numbers?
-// 3. What are the outputs that should come from the solution to the problrm?
+// 3. What are the outputs that should come from the solution to the problem?
 // - int? float? string?
 // 4. Can the outputs be determined from the input? In other words, do i have enough information to solve the problem?
 
