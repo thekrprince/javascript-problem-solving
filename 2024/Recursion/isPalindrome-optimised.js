3d317db4aa71899fc51034ce028c3fb561fc7de6
princekr96 <princekr700@gmail.com>
2024-02-12 22:33:29 +0530

    palindrome recursion

---
+++ 2024/Recursion/isPalindrome-optimised.js
@@ -0,0 +1,8 @@
+function isPalindrome(str) {
+  if (str.length <= 1) return true;
+  if (str[0] !== str[str.length - 1]) return false;
+  return isPalindrome(str.slice(1, -1));
+}
+
+const res = isPalindrome("nayan");
+console.log(res);
