97b18f590fcdcd2bbe5773c87f48b46a725ffebf
princekr96 <princekr700@gmail.com>
2024-03-16 20:05:20 +0530

    flattenArray

--- 2024/Recursion/flattenArray.js
+++ 2024/Recursion/flattenArray.js
@@ -3,4 +3,21 @@
  *
  **/
 
-function flatten() {}
+function flatten(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      newArray = newArray.concat(flatten(arr[i]));
+    } else {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+const res1 = flatten([1, 2, 3, [4, 5]]);
+const res2 = flatten([1, [2, [3, 4], [[5]]]]);
+const res3 = flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]);
+console.log(res1);
+console.log(res2);
+console.log(res3);
