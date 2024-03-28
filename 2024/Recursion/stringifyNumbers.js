80a1043bfb68eead4b72b23ff63ff503e683ae32
princekr96 <princekr700@gmail.com>
2024-03-28 23:09:59 +0530

    stringifyNumbers

--- 2024/Recursion/stringifyNumbers.js
+++ 2024/Recursion/stringifyNumbers.js
@@ -5,16 +5,32 @@ Recursion would be a great way to solve this!
 The exercise intends for you to create a new object with the numbers converted to strings, and not modify the original. Keep the original object unchanged.
 */
 
-function stringifyNumbers() {}
+function stringifyNumbers(obj) {
+  let newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      console.log(obj[key]);
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
 
-let obj = {
+let obj1 = {
   num: 1,
   test: [],
   data: {
     val: 4,
+    l: "r",
     info: {
       isRight: true,
       random: 66,
     },
   },
 };
+
+console.log(stringifyNumbers(obj1));
