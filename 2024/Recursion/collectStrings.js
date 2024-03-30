89d7822a79d7ed90ef38e478eab3e2a38ecaa2de
princekr96 <princekr700@gmail.com>
2024-03-30 16:29:55 +0530

    collectStrings

---
+++ 2024/Recursion/collectStrings.js
@@ -0,0 +1,33 @@
+/*
+Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string.
+*/
+
+function collectStrings(obj) {
+  let stringsArr = [];
+  for (var key in obj) {
+    if (typeof obj[key] === "string") {
+      stringsArr.push(obj[key]);
+    } else if (typeof obj[key] === "object") {
+      stringsArr = stringsArr.concat(collectStrings(obj[key]));
+    }
+  }
+  return stringsArr;
+}
+
+const obj1 = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
+
+console.log(collectStrings(obj1));
