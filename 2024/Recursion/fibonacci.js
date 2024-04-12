9a4b85aaa5618131ddc39110547c15a7c3ce167f
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2024-04-12 11:17:14 +0530

    Update fibonacci.js description

--- 2024/Recursion/fibonacci.js
+++ 2024/Recursion/fibonacci.js
@@ -1,3 +1,9 @@
+/*
+Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. 
+Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, 
+and where every number thereafter is equal to the sum of the previous two numbers.
+*/
+
 function fibonacci(num) {
   if (num <= 2) {
     return 1;
