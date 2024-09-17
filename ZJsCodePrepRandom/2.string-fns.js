3b57ef39f973ad5db938d488ca5a148cf19eb884
princekr96 <princekr700@gmail.com>
2024-09-17 17:29:01 +0530

    extras

--- ZJsCodePrepRandom/2.string-fns.js
+++ ZJsCodePrepRandom/2.string-fns.js
@@ -3,3 +3,5 @@ let str1 = "JavaScript is a programming language.";
 console.log(str1.charAt(2));
 console.log(str1.concat("\nLearn it wisely."));
 console.log(str1.lastIndexOf("e"));
+console.log(str1.slice(15, -5));
+console.log(str1.substring(11));
