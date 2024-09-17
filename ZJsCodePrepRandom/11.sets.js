b455d8b4436bb6b271f4ac35421ea3670768faee
princekr96 <princekr700@gmail.com>
2024-09-17 17:29:09 +0530

    set create

---
+++ ZJsCodePrepRandom/11.sets.js
@@ -0,0 +1,23 @@
+const s1 = new Set();
+
+s1.add("hey");
+s1.add("learn");
+s1.add("JS");
+
+console.log(s1);
+
+console.log(s1.has("hi"));
+console.log(s1.has("JS"));
+
+console.log(s1.keys());
+console.log(s1.values());
+
+console.log(s1.entries((s) => console.log(s)));
+
+console.log(s1.delete("hey"));
+console.log(s1);
+
+console.log(s1.size);
+
+s1.clear();
+console.log(s1);
