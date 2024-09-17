d4cc301b60933167256d5bb01f5dc97a4b34570f
princekr96 <princekr700@gmail.com>
2024-09-18 00:54:00 +0530

    two sum naive

---
+++ neetcode/1. Arrays & Hashing/4.twoSum.js
@@ -0,0 +1,23 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      const sum = nums[i] + nums[j];
+      if (sum === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 11, 4, 6, 27], 30));
+console.log(twoSum([3, 11, 4, 6, 27], 1));
