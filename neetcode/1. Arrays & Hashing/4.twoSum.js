45b626533431881c7590c6912211999cb0347e9e
princekr96 <princekr700@gmail.com>
2024-09-26 23:09:35 +0530

    2nd method

--- neetcode/1. Arrays & Hashing/4.twoSum.js
+++ neetcode/1. Arrays & Hashing/4.twoSum.js
@@ -20,4 +20,28 @@ var twoSum = function (nums, target) {
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 11, 4, 6, 27], 30));
-console.log(twoSum([3, 11, 4, 6, 27], 1));
+
+// Optimized solution
+
+function twoSum1(nums, target) {
+  let firstIdx = 0;
+  let secondIdx = 1;
+
+  while (secondIdx <= nums.length - 1) {
+    if (nums[firstIdx] + nums[secondIdx] === target) {
+      return [firstIdx, secondIdx];
+    } else if (secondIdx !== nums.length - 1) {
+      secondIdx++;
+    } else {
+      firstIdx++;
+      secondIdx = firstIdx + 1;
+    }
+  }
+
+  return;
+}
+
+console.log(twoSum1([2, 7, 11, 15], 9));
+console.log(twoSum1([3, 2, 4], 6));
+console.log(twoSum1([3, 11, 4, 6, 27], 30));
+console.log(twoSum1([2, 5, 5, 11], 10));
