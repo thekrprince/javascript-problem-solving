a1ff6e2f4c293f7584225bb6b1a6403bee31f74e
princekr96 <princekr700@gmail.com>
2024-10-06 14:41:13 +0530

    threeSum

--- neetcode/2.Two Pointers/3.threeSum.js
+++ neetcode/2.Two Pointers/3.threeSum.js
@@ -4,12 +4,46 @@ Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
 Notice that the solution set must not contain duplicate triplets.
  */
 
+function threeSum1(nums) {
+  nums.sort((a, b) => a - b);
+
+  let triplets = [];
+  console.log(nums);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+      let left = i + 1;
+      let right = nums.length - 1;
+
+      while (left < right) {
+        const currentSum = nums[i] + nums[left] + nums[right];
+        console.log(currentSum);
+        if (currentSum === 0) {
+          triplets.push([nums[i], nums[left], nums[right]]);
+          while (left < right && nums[left] === nums[left + 1]) left++;
+          while (left < right && nums[right] === nums[right - 1]) right--;
+          left++;
+          right--;
+          console.log("here");
+        } else if (currentSum < 0) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+  }
+  return triplets;
+}
+
+console.log(threeSum1([-1, 0, 1, 2, -1, -4]));
+console.log(threeSum1([-2, 0, 0, 2, 2]));
+
 // Just returning the true and false based on three pointers
 
 function threeSum(nums) {
   nums.sort((a, b) => a - b);
 
-  console.log(nums);
   for (let i = 0; i < nums.length - 2; i++) {
     let left = i + 1;
     let right = nums.length - 1;
@@ -20,7 +54,7 @@ function threeSum(nums) {
         return true;
       } else if (sum > 0) {
         right--;
-      } else if (sum < 0) {
+      } else {
         left++;
       }
     }
