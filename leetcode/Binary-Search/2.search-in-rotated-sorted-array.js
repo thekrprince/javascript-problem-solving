5f1791b113c1ac2ebe483031137e5e15eb9a2274
princekr96 <princekr700@gmail.com>
2025-02-16 16:35:51 +0530

    search in rotated sorted array

--- leetcode/Binary-Search/2.search-in-rotated-sorted-array.js
+++ leetcode/Binary-Search/2.search-in-rotated-sorted-array.js
@@ -8,3 +8,41 @@ You must write an algorithm with O(log n) runtime complexity.
 
 Link: https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 */
+
+function searchInRotatedSortedArray(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    console.log("left", left);
+    console.log("right", right);
+    console.log("mid", mid);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] >= nums[left]) {
+      if (nums[left] <= target && target <= nums[mid]) {
+        console.log(nums[left] <= target <= nums[mid]);
+        console.log(nums[left], target, nums[mid]);
+        console.log("first");
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+        console.log("second");
+      }
+    } else {
+      if (nums[mid] <= target && target <= nums[right]) {
+        left = mid + 1;
+        console.log("third");
+      } else {
+        right = mid - 1;
+        console.log("fourth");
+      }
+    }
+  }
+
+  return -1;
+}
+
+const res1 = searchInRotatedSortedArray([4, 5, 6, 7, 0, 1, 2], 0);
+console.log(res1);
