8335409da353588e513cf29e65326d1b065cac4c
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-03-09 07:26:06 +0000

    function counter

--- GFE/function-counter.js
+++ GFE/function-counter.js
@@ -3,3 +3,17 @@ Implement a function makeCounter that accepts an optional integer value and retu
 it returns the initial value if provided, otherwise 0.
 The returned function can be called repeatedly to return 1 more than the return value of the previous invocation.
 */
+
+function counter(initialVal = 0) {
+  let count = initialVal - 1;
+  return function () {
+    count += 1;
+    return count;
+  };
+}
+
+const count = counter(0);
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
