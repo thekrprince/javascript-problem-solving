96f8d1ba1037c7788d85525c6c3db06941db088e
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-03-09 07:27:04 +0000

    closure example

--- GFE/function-counter.js
+++ GFE/function-counter.js
@@ -2,6 +2,8 @@
 Implement a function makeCounter that accepts an optional integer value and returns a function. When the returned function is called initially,
 it returns the initial value if provided, otherwise 0.
 The returned function can be called repeatedly to return 1 more than the return value of the previous invocation.
+
+Note:- This solution is using the closure
 */
 
 function counter(initialVal = 0) {
@@ -17,3 +19,9 @@ console.log(count());
 console.log(count());
 console.log(count());
 console.log(count());
+
+const count1 = counter(5);
+console.log(count1());
+console.log(count1());
+console.log(count1());
+console.log(count1());
