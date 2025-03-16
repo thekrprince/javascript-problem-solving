c671e38a20295089846d1d98a47ceb4ecfdb3a6c
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-03-16 17:12:25 +0000

    dropRightWhile method

--- GFE/dropRightWhile.js
+++ GFE/dropRightWhile.js
@@ -1,9 +1,16 @@
 // Lodash's dropRightWhile function
 
 function dropRightWhile(array, predicate) {
+    let index = array.length - 1;
 
+    while(index >= 0 && predicate(array[index])){
+        index--;
+    }
+
+    return array.slice(0, index + 1);
 }
 
-dropRightWhile([1, 2, 3, 4, 5], (value) => value > 3); // => [1, 2, 3]
-dropRightWhile([1, 2, 3], (value) => value < 6); // => []
-dropRightWhile([1, 2, 3, 4, 5], (value) => value > 6); // => [1, 2, 3, 4, 5]
+console.log(dropRightWhile([1, 2, 3, 4, 5], (value) => value > 3)); // => [1, 2, 3]
+console.log(dropRightWhile([1, 2, 3], (value) => value < 6)); // => []
+console.log(dropRightWhile([1, 2, 3, 4, 5], (value) => value > 6)); // => [1, 2, 3, 4, 5]
+console.log(dropRightWhile([1, , 3, 4, 5], (value) => value === undefined))
