bdc22c57fb2d4e70c7b2ff845de0e87ee215f426
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-04-01 18:01:31 +0000

    added the inputs

--- GFE/fill.js
+++ GFE/fill.js
@@ -1,1 +1,9 @@
 // Lodash's fill method
+
+function fill(array, value, start = 0, end = array.length) {
+
+}
+
+console.log(fill(fill([1, 2, 3, 4, 5], '*', 2, 4));
+console.log(fill([1, 2], '*'));
+console.log(fill([1, 2, 3, 4, 5], '*', -1, -4));
