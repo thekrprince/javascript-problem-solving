475bd087aaecf6357870fc7563c0988eb1cc43c2
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-04-02 18:11:08 +0000

    findIndex of lodash

--- GFE/findIndex.js
+++ GFE/findIndex.js
@@ -1,7 +1,19 @@
 // Great Frontend - https://www.greatfrontend.com/questions/javascript/find-index?practice=practice&tab=coding
 
 function findIndex(array, predicate, fromIndex = 0) {
+    let i = fromIndex < 0 ? fromIndex + arrayLength : fromIndex;
 
+    while(i < array.length) {
+        const pred = predicate(array[i]);
+
+        if(pred) return i;
+
+        i++;
+    }
+
+    return -1;
 }
 
-console.log(findIndex());
+console.log(findIndex([], (num) => num % 2 === 0));
+console.log(findIndex([4, 12, 8, 130, 44], (num) => num % 2 === 0));
+console.log(findIndex([5, 12, 8, 130, 44], (num) => num % 2 === 0));
