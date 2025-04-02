9150bb4c5e345759874dc489a8f19cdd1efb3de1
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-04-02 18:03:03 +0000

    findIndex of GFE starter

---
+++ GFE/findIndex.js
@@ -0,0 +1,7 @@
+// Great Frontend - https://www.greatfrontend.com/questions/javascript/find-index?practice=practice&tab=coding
+
+function findIndex(array, predicate, fromIndex = 0) {
+
+}
+
+console.log(findIndex());
