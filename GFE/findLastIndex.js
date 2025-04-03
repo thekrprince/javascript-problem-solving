b7a6b1045bca48e777d08087969af28f31cf4906
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-04-03 18:05:56 +0000

    findLastIndex complete

--- GFE/findLastIndex.js
+++ GFE/findLastIndex.js
@@ -1,5 +1,23 @@
 // GFE - https://www.greatfrontend.com/questions/javascript/find-last-index?practice=practice&tab=coding
 
-function findLastIndex() {
-    
+function findLastIndex(array, predicate, fromIndex = array.length - 1) {
+    let length = array.length;
+
+    let startIndex = fromIndex < 0 ? Math.max(fromIndex + length, 0) : Math.min(fromIndex, length - 1);
+
+    while(startIndex >= 0) {
+        if(predicate(array[startIndex])) {
+            return startIndex;
+        }
+
+        startIndex--;
+    }
+
+    return -1;
 }
+
+const arr = [5, 4, 3, 2, 1];
+
+console.log(findLastIndex(arr, (num) => num > 3)); // => 1
+console.log(findLastIndex(arr, (num) => num > 1, 3)); // => 3
+console.log(findLastIndex(arr, (num) => num < 1, 3)); // => -1
