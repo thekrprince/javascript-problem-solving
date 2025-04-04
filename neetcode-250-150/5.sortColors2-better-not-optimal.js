ce476788ab44748dfcd60d41937e80873db2c55b
princekr96 <princekr700@gmail.com>
2025-04-04 22:58:25 +0530

    sortColors - not so optimal using 2O(N)

---
+++ neetcode-250-150/5.sortColors2-better-not-optimal.js
@@ -0,0 +1,12 @@
+// Sort Colors -> The optimal problem is solved in 4th number file
+
+function sortColors(arr) {}
+
+let arr1 = [2, 0, 2, 1, 1, 0];
+let arr2 = [2, 0, 1];
+
+sortColors(arr1);
+console.log(arr1);
+
+sortColors(arr2);
+console.log(arr2);
