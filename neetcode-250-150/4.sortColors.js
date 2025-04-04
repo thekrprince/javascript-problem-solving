1ff824fed938fd0750dca019cceb04feba205730
princekr96 <princekr700@gmail.com>
2025-04-04 22:56:10 +0530

    added comment

--- neetcode-250-150/4.sortColors.js
+++ neetcode-250-150/4.sortColors.js
@@ -1,6 +1,7 @@
 // Sort Colors -> Also known as Sort 0, 1, and 2 - link: https://leetcode.com/problems/sort-colors/
 // Video link to understand - https://www.youtube.com/watch?v=tp8JIuCXBaU by Tuf
 
+/* Solved using Dutch National Flag Algorithm */
 function sortColors(arr) {
   let low = 0;
   let mid = 0;
