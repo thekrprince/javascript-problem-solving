59bf0dbf9dde82f23c9907c7c12f85c66f1a9807
princekr96 <princekr700@gmail.com>
2025-04-06 16:52:38 +0530

    majorityElement2 completed

--- neetcode-250-150/7.majorityElement2.js
+++ neetcode-250-150/7.majorityElement2.js
@@ -1,2 +1,31 @@
 // Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times
 // https://leetcode.com/problems/majority-element-ii/
+
+function majorityElement(nums) {
+  const countRequired = Math.floor(nums.length / 3) + 1;
+  const obj = {};
+  let majorityEle = [];
+
+  for (let val of nums) {
+    // if(obj[val]) {
+    obj[val] = (obj[val] || 0) + 1;
+    if (obj[val] === countRequired) {
+      majorityEle.push(val);
+    }
+    // }
+  }
+
+  return majorityEle;
+}
+
+const nums = [3, 2, 3];
+console.log(majorityElement(nums));
+
+const nums2 = [1];
+console.log(majorityElement(nums2));
+
+const nums3 = [1, 2];
+console.log(majorityElement(nums3));
+
+const nums4 = [1, 1, 1, 2, 3, 2, 1, 2];
+console.log(majorityElement(nums4));
