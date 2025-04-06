1adbb19b0b39459f46eef72f27155a1541bc5e9a
princekr96 <princekr700@gmail.com>
2025-04-06 16:30:21 +0530

    code done

---
+++ neetcode-250-150/6.bestTimeToBuyNSellStocks2.js
@@ -0,0 +1,17 @@
+//
+
+function maxProfit(prices) {
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+
+  return profit;
+}
+
+const prices = [7, 1, 5, 3, 6, 4];
+
+console.log(maxProfit(prices));
