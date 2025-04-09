6d8137002a83d4c626f3140fb246c658b22b9c90
princekr96 <princekr700@gmail.com>
2025-04-09 10:12:47 +0530

    subArraySumEqualsK starter

---
+++ neetcode-250-150/10.subArraySumEqualsK.js
@@ -0,0 +1,3 @@
+// Subarray Sum Equals K - link: https://leetcode.com/problems/subarray-sum-equals-k/description/
+/* Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+   A subarray is a contiguous non-empty sequence of elements within an array. */
