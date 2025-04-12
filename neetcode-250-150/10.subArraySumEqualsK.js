e070972e0558cb228b3dab80e057131054028921
princekr96 <princekr700@gmail.com>
2025-04-12 15:32:04 +0530

    comment

--- neetcode-250-150/10.subArraySumEqualsK.js
+++ neetcode-250-150/10.subArraySumEqualsK.js
@@ -2,6 +2,7 @@
 /* Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
    A subarray is a contiguous non-empty sequence of elements within an array. */
 
+// Solved using O(N2) - Optimal is in another one
 function subarraySumEqualsK(nums, k) {
   let count = 0;
 
