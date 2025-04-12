0ba35c003a898f4306a0fcada36fe4bc16436524
princekr96 <princekr700@gmail.com>
2025-04-12 15:37:18 +0530

    optimal starter

---
+++ neetcode-250-150/11.subArraySumEqualsK-optimal.js
@@ -0,0 +1,9 @@
+function subArraySumEqualsK(nums, k) {}
+
+const input1 = [1, -1, 1, 1, 1];
+const input2 = [1, 1, 1];
+const input3 = [1, 2, 3];
+
+subArraySumEqualsK(input1, 3);
+subArraySumEqualsK(input2, 2);
+subArraySumEqualsK(input3, 3);
