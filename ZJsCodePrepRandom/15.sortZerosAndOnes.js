b6c6ca818776237573bf9ff981f281b93b196c34
princekr96 <princekr700@gmail.com>
2025-04-12 13:48:27 +0530

    solved

--- ZJsCodePrepRandom/15.sortZerosAndOnes.js
+++ ZJsCodePrepRandom/15.sortZerosAndOnes.js
@@ -1,6 +1,28 @@
 // Sort 0's and 1's in an array
 
-function sortZeroesAndOnes(nums) {}
+function sortZeroesAndOnes(nums) {
+  let i = 0,
+    j = nums.length - 1;
+
+  while (i < j) {
+    if (nums[i] > nums[j]) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      i++;
+      j--;
+    } else if (nums[i] === 0 && nums[j] === 0) {
+      i++;
+    } else {
+      i++;
+      j--;
+    }
+  }
+
+  console.log(nums);
+}
 
 const input1 = [0, 1, 0, 1, 1, 0, 0];
-console.log(sortZeroesAndOnes(input1));
+const input2 = [1, 1, 1, 0, 0, 0];
+const input3 = [1, 0, 1, 0, 1, 0];
+sortZeroesAndOnes(input1);
+sortZeroesAndOnes(input2);
+sortZeroesAndOnes(input3);
