136b37b1edf3ed1ea78c80cb31a0accc68a669bb
princekr96 <princekr700@gmail.com>
2025-04-13 15:51:53 +0530

    validPalindrome2 starter

---
+++ neetcode-250-150/2. Two Pointers/1.ValidPalindrome2.js
@@ -0,0 +1,7 @@
+// Valid Palindrome 2 - https://leetcode.com/problems/valid-palindrome-ii/description/
+
+function validPalindrome(s) {}
+
+let str1 = "abca";
+let str2 = "abdda";
+let str3 = "deeeee";
