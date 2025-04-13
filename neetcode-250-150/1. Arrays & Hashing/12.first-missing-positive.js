b45d6e3223bc65e4aeaabd9bcb2188a0e78dfe7f
princekr96 <princekr700@gmail.com>
2025-04-13 15:53:41 +0530

    first missing positive starter

---
+++ neetcode-250-150/1. Arrays & Hashing/12.first-missing-positive.js
@@ -0,0 +1,7 @@
+// First Missing Positive - https://leetcode.com/problems/first-missing-positive/
+
+function firstMissingPositive(nums) {}
+
+const nums1 = [1, 2, 0];
+const nums2 = [3, 4, -1, 1];
+const nums3 = [[7, 8, 9, 11, 12]];
