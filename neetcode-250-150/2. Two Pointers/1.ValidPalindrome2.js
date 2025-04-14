64f3e0b133cbd490a088bda210472377392da2f6
princekr96 <princekr700@gmail.com>
2025-04-14 23:27:37 +0530

    first commit

--- neetcode-250-150/2. Two Pointers/1.ValidPalindrome2.js
+++ neetcode-250-150/2. Two Pointers/1.ValidPalindrome2.js
@@ -1,7 +1,42 @@
 // Valid Palindrome 2 - https://leetcode.com/problems/valid-palindrome-ii/description/
 
-function validPalindrome(s) {}
+const isPalindrome = (s) => {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+};
+
+function validPalindrome(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      let leftSkip = s.substr(left + 1, right);
+      let rightSkip = s.substr(left, right - 1);
+
+      return isPalindrome(leftSkip) || isPalindrome(rightSkip);
+    }
+
+    left++;
+    right--;
+  }
+}
 
 let str1 = "abca";
+console.log(validPalindrome(str1));
+
 let str2 = "abdda";
+console.log(validPalindrome(str2));
+
 let str3 = "deeeee";
+console.log(validPalindrome(str3));
