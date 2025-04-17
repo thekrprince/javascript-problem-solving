f8b560d89aec5203b245e80882c5b0ae15fa0d00
princekr96 <princekr700@gmail.com>
2025-04-17 23:38:39 +0530

    completed with an example

--- neetcode-250-150/2. Two Pointers/2.MergeSortedArray.js
+++ neetcode-250-150/2. Two Pointers/2.MergeSortedArray.js
@@ -1,6 +1,24 @@
 // Merge Two Sorted Arrays
 
-function mergeTwoSortedArrays(nums1, m, nums2, n) {}
+function mergeTwoSortedArrays(nums1, m, nums2, n) {
+  let last = m + n - 1;
+
+  while (m - 1 >= 0 && n - 1 >= 0) {
+    console.log("compare", nums1[m - 1], nums2[n - 1]);
+    if (nums1[m - 1] > nums2[n - 1]) {
+      nums1[last] = nums1[m - 1];
+      m -= 1;
+    } else {
+      nums1[last] = nums2[n - 1];
+      n -= 1;
+    }
+    last -= 1;
+  }
+
+  console.log(nums1);
+}
 
 let arr1 = [1, 2, 3, 0, 0, 0];
 let arr2 = [2, 5, 6];
+
+mergeTwoSortedArrays(arr1, 3, arr2, 3);
