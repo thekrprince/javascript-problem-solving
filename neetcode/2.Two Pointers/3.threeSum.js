de62ebed1d4350c047fe1218b8bd6916bb2e85c2
princekr96 <princekr700@gmail.com>
2025-04-18 17:46:38 +0530

    removed unnecessary console

--- neetcode/2.Two Pointers/3.threeSum.js
+++ neetcode/2.Two Pointers/3.threeSum.js
@@ -8,7 +8,6 @@ function threeSum1(nums) {
   nums.sort((a, b) => a - b);
 
   let triplets = [];
-  console.log(nums);
 
   for (let i = 0; i < nums.length - 2; i++) {
     if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
@@ -17,14 +16,12 @@ function threeSum1(nums) {
 
       while (left < right) {
         const currentSum = nums[i] + nums[left] + nums[right];
-        console.log(currentSum);
         if (currentSum === 0) {
           triplets.push([nums[i], nums[left], nums[right]]);
           while (left < right && nums[left] === nums[left + 1]) left++;
           while (left < right && nums[right] === nums[right - 1]) right--;
           left++;
           right--;
-          console.log("here");
         } else if (currentSum < 0) {
           left++;
         } else {
@@ -62,6 +59,6 @@ function threeSum(nums) {
   return false;
 }
 
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
-console.log(threeSum([11, 7, 5, 9, 8, -6, 16, 17, -3]));
-console.log(threeSum([5, 7, 9, 9, 8, -10, 16, 8, -3]));
+// console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+// console.log(threeSum([11, 7, 5, 9, 8, -6, 16, 17, -3]));
+// console.log(threeSum([5, 7, 9, 9, 8, -10, 16, 8, -3]));
