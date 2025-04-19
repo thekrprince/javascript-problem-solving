742bffc0285d4f4f3076de6ae575b3c0eaaaa5f1
princekr96 <princekr700@gmail.com>
2025-04-19 18:51:35 +0530

    first solution

--- neetcode-250-150/2. Two Pointers/3.RotateArray.js
+++ neetcode-250-150/2. Two Pointers/3.RotateArray.js
@@ -1,5 +1,15 @@
 // File: 3.RotateArray.js
 
-function rotateArray() {
+function rotateArray(nums, k) {
   // TODO: Implement the function
+  let res = new Array(nums.length).fill(0);
+  for (let i = 0; i < nums.length; i++) {
+    let findIndexToPlace = (i + k) % nums.length;
+    res[findIndexToPlace] = nums[i];
+  }
+
+  console.log(res);
 }
+
+rotateArray([1, 2, 3, 4, 5, 6, 7], 3); // [5, 6, 7, 1, 2, 3, 4]
+rotateArray([-1, -100, 3, 99], 2); // [3, 99, -1, -100]
