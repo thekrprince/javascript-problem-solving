35052ae0477c97f3fba130b0e2f6c0f3c6c65abd
princekr96 <princekr700@gmail.com>
2025-04-24 22:34:38 +0530

    example added to solve later

--- neetcode-250-150/2. Two Pointers/4.BoatsToSavePeople.js
+++ neetcode-250-150/2. Two Pointers/4.BoatsToSavePeople.js
@@ -1,3 +1,8 @@
 // Boats to Save People
 
 function numRescueBoats(people, limit) {}
+
+console.log(numRescueBoats([1, 2], 3));
+console.log(numRescueBoats([3, 2, 2, 1], 3));
+console.log(numRescueBoats([3, 5, 3, 4], 5));
+console.log(numRescueBoats([5, 1, 4, 2], 6));
