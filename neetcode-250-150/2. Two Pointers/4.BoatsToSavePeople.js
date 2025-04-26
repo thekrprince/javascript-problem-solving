0319b42424e978d48ac661a5c4c1ebf0c90cc8f0
princekr96 <princekr700@gmail.com>
2025-04-26 23:18:41 +0530

    boat to save people

--- neetcode-250-150/2. Two Pointers/4.BoatsToSavePeople.js
+++ neetcode-250-150/2. Two Pointers/4.BoatsToSavePeople.js
@@ -1,6 +1,29 @@
 // Boats to Save People
 
-function numRescueBoats(people, limit) {}
+function numRescueBoats(people, limit) {
+  people.sort((a, b) => a - b);
+
+  let l = 0,
+    r = people.length - 1;
+  let boatRequired = 0;
+
+  while (l < r) {
+    if (people[l] + people[r] <= limit) {
+      boatRequired++;
+      l++;
+      r--;
+    } else {
+      boatRequired++;
+      r--;
+    }
+  }
+
+  if (l === r) {
+    boatRequired++;
+  }
+
+  return boatRequired;
+}
 
 console.log(numRescueBoats([1, 2], 3));
 console.log(numRescueBoats([3, 2, 2, 1], 3));
