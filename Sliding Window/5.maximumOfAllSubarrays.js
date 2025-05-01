3ef5d590654472a8d00abcfbfacf482919d2beb2
princekr96 <princekr700@gmail.com>
2025-05-01 20:05:26 +0530

    solved using deque approach

--- Sliding Window/5.maximumOfAllSubarrays.js
+++ Sliding Window/5.maximumOfAllSubarrays.js
@@ -4,28 +4,24 @@ function maximumOfAllSubArrays(nums, k) {
   let i = 0;
   let j = 0;
   let res = [];
-  let tempNumsOfSizeK = [];
+  let deque = [];
 
   while (j < nums.length) {
-    tempNumsOfSizeK.push(nums[j]);
+    while (deque.length > 0 && deque[deque.length - 1] < nums[j]) {
+      deque.pop();
+    }
+
+    deque.push(nums[j]);
+
     if (j - i + 1 < k) {
       j++;
     } else if (j - i + 1 === k) {
-      console.log("tempNumsOfSizeK", tempNumsOfSizeK);
-      if (
-        tempNumsOfSizeK[0] > tempNumsOfSizeK[2] &&
-        tempNumsOfSizeK[0] > tempNumsOfSizeK[1]
-      ) {
-        res.push(tempNumsOfSizeK[0]);
-      } else if (
-        tempNumsOfSizeK[1] > tempNumsOfSizeK[2] &&
-        tempNumsOfSizeK[1] > tempNumsOfSizeK[0]
-      ) {
-        res.push(tempNumsOfSizeK[1]);
-      } else {
-        res.push(tempNumsOfSizeK[2]);
+      res.push(deque[0]);
+
+      if (deque[0] === nums[i]) {
+        deque.shift();
       }
-      tempNumsOfSizeK.splice(0, 1);
+
       i++;
       j++;
     }
