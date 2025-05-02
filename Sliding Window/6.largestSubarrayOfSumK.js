6b62485c2757850463d95faf3fd7bb9f1ee02327
princekr96 <princekr700@gmail.com>
2025-05-02 21:25:17 +0530

    renamed

---
+++ Sliding Window/6.largestSubarrayOfSumK.js
@@ -0,0 +1,5 @@
+// Longest subarray of sum k
+
+function largestSubArrayOfSumK(arr, sum) {}
+
+console.log(largestSubArrayOfSumK([4, 1, 1, 1, 2, 3, 5], 5));
--- Sliding Window/6.longestSubarrayOfSumK.js
+++
@@ -1,7 +0,0 @@
-// Longest subarray of sum k
-
-function longestSubArrayOfSumK(arr, sum) {
-
-}
-
-console.log(longestSubArrayOfSumK([4,1,1,1,2,3,5], 5))
