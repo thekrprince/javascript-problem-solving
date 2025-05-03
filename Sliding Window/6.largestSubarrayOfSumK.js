22010d7c6937749c4ddd2f6e68463ff23f5dca51
princekr96 <princekr700@gmail.com>
2025-05-03 14:59:09 +0530

    largest subrray of sum k v1.0

--- Sliding Window/6.largestSubarrayOfSumK.js
+++ Sliding Window/6.largestSubarrayOfSumK.js
@@ -1,5 +1,29 @@
 // Longest subarray of sum k
 
-function largestSubArrayOfSumK(arr, sum) {}
+function largestSubArrayOfSumK(arr, k) {
+  let i = 0;
+  let j = 0;
+
+  let sum = 0;
+  let max = 0;
+
+  while (j < arr.length) {
+    sum += arr[j];
+
+    if (sum < k) {
+      j++;
+    } else if (sum === k) {
+      max = Math.floor(j - i + 1, max);
+      j++;
+    } else {
+      sum -= arr[i];
+      i++;
+      j++;
+    }
+  }
+
+  return max;
+}
 
 console.log(largestSubArrayOfSumK([4, 1, 1, 1, 2, 3, 5], 5));
+console.log(largestSubArrayOfSumK([1, 2, 1, 0, 1, 1, 1], 4));
