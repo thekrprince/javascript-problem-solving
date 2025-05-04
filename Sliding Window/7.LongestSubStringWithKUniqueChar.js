6d54cf3a94e7a6def83ea487a208a7242da55aa4
princekr96 <princekr700@gmail.com>
2025-05-04 12:45:31 +0530

    added an extra solution for this

--- Sliding Window/7.LongestSubStringWithKUniqueChar.js
+++ Sliding Window/7.LongestSubStringWithKUniqueChar.js
@@ -32,3 +32,44 @@ function longestSubstringWithUniqueCharacter(str, k) {
 }
 
 console.log(longestSubstringWithUniqueCharacter("aabacbebebe", 3));
+
+// Same code with little optimization
+function longestSubstringWithUniqueCharacter1(str, k) {
+  let i = 0;
+  let j = 0;
+  let longestSub = 0;
+  let uniqueCount = 0;
+
+  let map = {};
+
+  while (j < str.length) {
+    const char = str[j];
+    map[char] = (map[char] || 0) + 1;
+
+    if (map[char] === 1) uniqueCount++;
+
+    if (uniqueCount < k) {
+      j++;
+    } else if (uniqueCount === k) {
+      longestSub = Math.max(longestSub, j - i + 1);
+      j++;
+    } else {
+      while (uniqueCount > k) {
+        const leftChar = str[i];
+        map[leftChar] = map[leftChar] - 1;
+        if (map[leftChar] === 0) {
+          delete map[leftChar];
+          uniqueCount--;
+        }
+        i++;
+      }
+      j++;
+    }
+  }
+
+  return longestSub;
+}
+
+console.log(longestSubstringWithUniqueCharacter1("aabacbebebe", 3));
+console.log(longestSubstringWithUniqueCharacter1("aaaa", 1));
+console.log(longestSubstringWithUniqueCharacter1("abcba", 2));
