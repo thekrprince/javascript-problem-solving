3411ddaaaad75759e107c844fc3008e15ca44ae0
princekr96 <princekr700@gmail.com>
2025-05-11 14:42:27 +0530

    longest substring

--- neetcode-250-150/3. Sliding Window/3.longestSubstringWithoutRepChar.js
+++ neetcode-250-150/3. Sliding Window/3.longestSubstringWithoutRepChar.js
@@ -1,1 +1,33 @@
-// Longest Substring Without Repeating Characters
+// Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-repeating-character-replacement/
+
+function longestSubstringWithoutRepChar(s) {
+  let i = 0;
+  let longestSub = 0;
+  let map = {};
+
+  for (let j = 0; j < s.length; j++) {
+    const rightChar = s[j];
+    map[rightChar] = (map[rightChar] || 0) + 1;
+    let mapSize = Object.keys(map).length;
+
+    if (j - i + 1 === mapSize) {
+      longestSub = Math.max(longestSub, j - i + 1);
+    } else if (j - i + 1 > mapSize) {
+      while (j - i + 1 > mapSize) {
+        const leftChar = s[i];
+        map[leftChar]--;
+
+        if (map[leftChar] === 0) {
+          delete map[leftChar];
+        }
+        i++;
+      }
+    }
+  }
+
+  return longestSub;
+}
+
+console.log(longestSubstringWithoutRepChar("zxyzxyz"));
+console.log(longestSubstringWithoutRepChar("xxxxx"));
+console.log(longestSubstringWithoutRepChar("pwwkew"));
