b0aa9b2b8c1faaae403c9e6448e1910b81684061
princekr96 <princekr700@gmail.com>
2025-05-19 23:33:17 +0530

    starter

---
+++ neetcode-250-150/3. Sliding Window/7.findKClosestElements.js
@@ -0,0 +1,6 @@
+// Find K Closest Elements - https://leetcode.com/problems/find-k-closest-elements/
+
+function findKClosestElements(arr, k, x) {}
+
+console.log(findKClosestElements([1, 2, 3, 4, 5], 4, 3));
+console.log(findKClosestElements([1, 1, 2, 3, 4, 5], 4, -1));
