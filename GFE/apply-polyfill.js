cfcbea1eb87162ec08a22e5f707d2b33bed7d196
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-05-28 23:44:06 +0530

    Update apply-polyfill.js

--- GFE/apply-polyfill.js
+++ GFE/apply-polyfill.js
@@ -1,1 +1,20 @@
 // https://www.greatfrontend.com/questions/javascript/function-apply?practice=practice&tab=coding
+
+function printInConsole() {
+  console.log(this.name);
+}
+
+Function.prototype.myApply = function (thisArg, argArray) {
+  thisArg = thisArg || globalThis;
+
+  let uniqueFn = Symbol("fn");
+  thisArg[uniqueFn] = this;
+
+  return argArray ? thisArg[uniqueFn](...argArray) : thisArg[uniqueFn]();
+};
+
+let obj = {
+  name: "greatFrontend",
+};
+
+printInConsole.myApply(obj);
