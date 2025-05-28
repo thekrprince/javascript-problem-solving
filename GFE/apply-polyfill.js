c14a5189c81a4aedbae9b14001a5e9cbb960098f
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-05-28 23:44:57 +0530

    added one more fun

--- GFE/apply-polyfill.js
+++ GFE/apply-polyfill.js
@@ -4,6 +4,11 @@ function printInConsole() {
   console.log(this.name);
 }
 
+function multiply(multiplier = 2, ...anotherNum) {
+  console.log(anotherNum);
+  return this.number * multiplier;
+}
+
 Function.prototype.myApply = function (thisArg, argArray) {
   thisArg = thisArg || globalThis;
 
@@ -15,6 +20,8 @@ Function.prototype.myApply = function (thisArg, argArray) {
 
 let obj = {
   name: "greatFrontend",
+  number: 3
 };
 
 printInConsole.myApply(obj);
+console.log(multiply.myApply(obj, [6, 7, 8]));
