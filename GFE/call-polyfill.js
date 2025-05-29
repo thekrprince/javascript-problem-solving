d116de791d35d5863b555ef03fd81426352a3eb7
Kumar Prince <39616644+thekrprince@users.noreply.github.com>
2025-05-29 18:56:31 +0000

    call polyfill

--- GFE/call-polyfill.js
+++ GFE/call-polyfill.js
@@ -1,3 +1,20 @@
 // https://www.greatfrontend.com/questions/javascript/function-call?practice=practice&tab=coding
+function multiplyLuckyNum() {
+  return this.num * 100;
+}
 
-Function.prototype.myCall = function () {};
+Function.prototype.myCall = function (thisArg, ...args) {
+  thisArg = thisArg || globalThis;
+
+  let tempFn = "_tempFn";
+  thisArg[tempFn] = this;
+
+  return args ? thisArg[tempFn](...args) : thisArg[tempFn]();
+};
+
+let obj1 = {
+  name: "Prince",
+  num: 9,
+};
+
+console.log(multiplyLuckyNum.myCall(obj1, "hello", "world"));
