902dfd9a32e13efa23c78279fff1716801d06ba6
princekr96 <princekr700@gmail.com>
2025-06-15 12:50:37 +0530

    removed the unoptimal solution

--- neetcode-250/2. Two Pointers/3.RotateArray.js
+++ neetcode-250/2. Two Pointers/3.RotateArray.js
@@ -19,18 +19,3 @@ function rotateArray1(nums, k) {
 
 rotateArray([1, 2, 3, 4, 5, 6, 7], 3); // [5, 6, 7, 1, 2, 3, 4]
 rotateArray([-1, -100, 3, 99], 2); // [3, 99, -1, -100]
-
-// This is the okay solution as the recommendation is to modify array in-place
-function rotateArray(nums, k) {
-  let res = new Array(nums.length).fill(0);
-  for (let i = 0; i < nums.length; i++) {
-    // This below formula will take the required index when it's out of bound.
-    let findIndexToPlace = (i + k) % nums.length;
-    res[findIndexToPlace] = nums[i];
-  }
-
-  console.log(res);
-}
-
-rotateArray([1, 2, 3, 4, 5, 6, 7], 3); // [5, 6, 7, 1, 2, 3, 4]
-rotateArray([-1, -100, 3, 99], 2); // [3, 99, -1, -100]
