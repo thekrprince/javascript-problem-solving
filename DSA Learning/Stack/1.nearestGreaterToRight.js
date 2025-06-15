ee9d2c6520a51c922dbc8c7e6cd74b5dab924b9d
princekr96 <princekr700@gmail.com>
2025-06-15 22:56:04 +0530

    nearestGreaterToRight

--- DSA Learning/Stack/1.nearestGreaterToRight.js
+++ DSA Learning/Stack/1.nearestGreaterToRight.js
@@ -1,1 +1,31 @@
 // Nearest greater to right - https://www.youtube.com/watch?v=NXOOYYwpbg4&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=2
+
+function findNearestGreaterInRight(arr) {
+  let stack = [];
+  let res = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (stack.length === 0) {
+      res.push(-1);
+    } else if (stack.length > 0 && stack[stack.length - 1] > arr[i]) {
+      res.push(stack[stack.length - 1]);
+    } else if (stack.length > 0 && stack[stack.length - 1] <= arr[i]) {
+      while (stack[stack.length - 1] <= arr[i]) {
+        stack.pop();
+      }
+
+      if (stack.length === 0) {
+        res.push(-1);
+      } else {
+        res.push(stack[stack.length - 1]);
+      }
+    }
+
+    stack.push(arr[i]);
+  }
+
+  return res.reverse();
+}
+
+const res1 = findNearestGreaterInRight([1, 3, 2, 4]);
+console.log(res1);
