dd39a280f1b8de10333b731e3c1635629740625e
princekr96 <princekr700@gmail.com>
2025-06-17 22:31:43 +0530

    stock span starter

---
+++ DSA Learning/Stack/5.stockSpanProblem.js
@@ -0,0 +1,5 @@
+// Stock Span Problem - https://www.youtube.com/watch?v=p9T-fE1g1pU&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=6
+
+function stockSpanProblem(arr) {}
+
+console.log(res([100, 80, 60, 70, 60, 75, 85]));
