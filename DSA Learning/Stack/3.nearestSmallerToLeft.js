68922df2e2dda77d13032aa82b5fa52fc47c112e
princekr96 <princekr700@gmail.com>
2025-06-17 21:02:17 +0530

    nearest smaller to left starter

---
+++ DSA Learning/Stack/3.nearestSmallerToLeft.js
@@ -0,0 +1,5 @@
+// Nearest smaller to left - https://www.youtube.com/watch?v=T5s96ynzArg&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=3
+
+function nearestSmallerToLeft(arr) {}
+
+console.log(nearestSmallerToLeft([4, 5, 2, 10, 8]));
