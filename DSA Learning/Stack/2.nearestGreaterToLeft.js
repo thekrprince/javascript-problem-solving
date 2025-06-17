25e512798ceb55e4baf10dc06204ed5fb61803b6
princekr96 <princekr700@gmail.com>
2025-06-17 20:56:51 +0530

    done

--- DSA Learning/Stack/2.nearestGreaterToLeft.js
+++ DSA Learning/Stack/2.nearestGreaterToLeft.js
@@ -1,6 +1,30 @@
 // Nearest Greater to left - https://www.youtube.com/watch?v=T5s96ynzArg&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=3
 
-function nearestGreaterToRight(arr) {}
+function nearestGreaterToLeft(arr) {
+  let stack = [];
+  let res = [];
 
-const res1 = findNearestGreaterInRight([1, 3, 2, 4]);
+  for (let i = 0; i <= arr.length; i++) {
+    if (stack.length === 0) {
+      res.push(-1);
+    } else if (stack[stack.length - 1] > arr[i]) {
+      res.push(stack[stack.length - 1]);
+    } else if (stack[stack.length - 1] <= arr[i]) {
+      while (stack[stack.length - 1] <= arr[i]) {
+        stack.pop();
+      }
+      if (stack.length === 0) {
+        res.push(-1);
+      } else {
+        res.push(stack[stack.length - 1]);
+      }
+    }
+
+    stack.push(arr[i]);
+  }
+
+  return res;
+}
+
+const res1 = nearestGreaterToLeft([4, 2, 3, 1]);
 console.log(res1);
