d60bb3558fbbd6378bf1997cfe2eec469a2c10da
princekr96 <princekr700@gmail.com>
2025-06-17 23:00:26 +0530

    stock span done

--- DSA Learning/Stack/5.stockSpanProblem.js
+++ DSA Learning/Stack/5.stockSpanProblem.js
@@ -1,5 +1,28 @@
 // Stock Span Problem - https://www.youtube.com/watch?v=p9T-fE1g1pU&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=6
 
-function stockSpanProblem(arr) {}
+function stockSpanProblem(arr) {
+  let stack = [];
+  let res = [];
 
-console.log(res([100, 80, 60, 70, 60, 75, 85]));
+  for (let i = 0; i < arr.length; i++) {
+    while (stack.length > 0 && stack[stack.length - 1][0] <= arr[i]) {
+      stack.pop();
+    }
+
+    if (stack.length === 0) {
+      res.push(-1);
+    } else {
+      res.push(stack[stack.length - 1][1]);
+    }
+
+    stack.push([arr[i], i]);
+  }
+
+  for (let j = 0; j < arr.length; j++) {
+    res[j] = j - res[j];
+  }
+
+  return res;
+}
+
+console.log(stockSpanProblem([100, 80, 60, 70, 60, 75, 85]));
