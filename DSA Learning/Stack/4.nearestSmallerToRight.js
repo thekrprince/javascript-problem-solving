b341b53cce454e054fb88a20292ad7ede94badda
princekr96 <princekr700@gmail.com>
2025-06-17 21:16:06 +0530

    nearest smaller to right starter

---
+++ DSA Learning/Stack/4.nearestSmallerToRight.js
@@ -0,0 +1,6 @@
+// Nearest Smaller to Right - https://www.youtube.com/watch?v=nc1AYFyvOR4&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=5
+
+function nearestSmallerToRight(arr) {}
+
+console.log(nearestSmallerToRight([4, 5, 2, 10, 8]));
+console.log(nearestSmallerToRight([2, 1]));
