0305c4a9adbf4b30e791e6eddc7c69f0cd22262d
princekr96 <princekr700@gmail.com>
2025-06-17 20:48:27 +0530

    starter empty fn

---
+++ DSA Learning/Stack/2.nearestGreaterToLeft.js
@@ -0,0 +1,6 @@
+// Nearest Greater to left - https://www.youtube.com/watch?v=T5s96ynzArg&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=3
+
+function nearestGreaterToRight(arr) {}
+
+const res1 = findNearestGreaterInRight([1, 3, 2, 4]);
+console.log(res1);
