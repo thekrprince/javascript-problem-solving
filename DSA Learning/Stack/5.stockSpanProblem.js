893dcfae8fa034b852b91a53ae3fa6377c4e622b
princekr96 <princekr700@gmail.com>
2025-06-21 23:05:38 +0530

    updated cmt

--- DSA Learning/Stack/5.stockSpanProblem.js
+++ DSA Learning/Stack/5.stockSpanProblem.js
@@ -1,4 +1,5 @@
 // Stock Span Problem - https://www.youtube.com/watch?v=p9T-fE1g1pU&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=6
+// For each day in arr, find the number of consecutive days before it (including itself) where the price was less than or equal to today's price.
 
 function stockSpanProblem(arr) {
   let stack = [];
