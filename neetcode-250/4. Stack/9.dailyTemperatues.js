a687949a97945219581c61a197c28d43d9d225d8
princekr96 <princekr700@gmail.com>
2025-06-30 23:33:55 +0530

    not working solution

--- neetcode-250/4. Stack/9.dailyTemperatues.js
+++ neetcode-250/4. Stack/9.dailyTemperatues.js
@@ -1,1 +1,22 @@
 //  LC 739 - https://leetcode.com/problems/daily-temperatures/description/
+
+function dailyTemperatures(temperatures) {
+  let stack = [];
+  let res = new Array(temperatures.length).fill(0);
+
+  for (let i = temperatures.length; i >= 0; i--) {
+    while (stack.length > 0 && stack[stack.length - 1] <= temperatures[i]) {
+      stack.pop();
+    }
+
+    if (stack.length > 0) {
+      res[i] = stack[stack.length - 1][1] - i;
+    }
+
+    stack.push([temperatures[i], i]);
+  }
+
+  return res;
+}
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
