cfa303d8ac182e037135ee50f326e63e3c05c613
princekr96 <princekr700@gmail.com>
2025-07-18 07:39:08 +0530

    Promise.any

---
+++ JavaScript/Promises/4. Promise.any.js
@@ -0,0 +1,21 @@
+// Similar to Promise.race, but waits only for the first fulfilled promise and gets its result.
+// If all of the given promises are rejected,
+// then the returned promise is rejected with AggregateError – a special error object that stores all promise errors in its errors property.
+
+let p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P1 Success"), 3000);
+});
+
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P2 Success"), 2000);
+});
+
+let p3 = new Promise((resolve, reject) => {
+  setTimeout(() => reject("P3 Fails"), 1000);
+});
+
+Promise.any([p1, p2, p3])
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+// The first promise here was fastest, but it was rejected, so the second promise became the result.
+// After the first fulfilled promise “wins the race”, all further results are ignored.
