3fd6aa3e8d1f5c86c4102d185a32f7abaf9766ef
princekr96 <princekr700@gmail.com>
2025-07-18 06:08:47 +0530

    updated the explanation

--- JavaScript/Promises/2. promises.allSettled.js
+++ JavaScript/Promises/2. promises.allSettled.js
@@ -1,4 +1,5 @@
 // Promise.allSettled - Takes iterable and return the result for all the Promises upon settlement of all irrespective of the result.
+//  The resulting array has: {status:"fulfilled", value:result} for successful responses, and {status:"rejected", reason:error} for errors.
 
 const p1 = new Promise((resolve, reject) => {
   //   setTimeout(() => resolve("P1 success"), 2000);
@@ -16,3 +17,12 @@ const p3 = new Promise((resolve, reject) => {
 const promise = Promise.allSettled([p1, p2, p3])
   .then((res) => console.log(res))
   .catch((err) => console.error(err));
+
+/*
+  O/P:
+  [
+    { status: 'rejected', reason: 'P1 error' },
+    { status: 'fulfilled', value: 'P1 success' },
+    { status: 'fulfilled', value: 'P1 success' }
+  ]
+*/
