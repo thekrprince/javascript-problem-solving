88c75ed147bd423b2d4fbbe1d53cac175d8013f1
princekr96 <princekr700@gmail.com>
2025-07-20 16:34:27 +0530

    improvement

--- JavaScript/Polyfills/13. Apply.js
+++ JavaScript/Polyfills/13. Apply.js
@@ -8,13 +8,18 @@ function describeCity(speciality) {
   console.log(`${this.name} is ${speciality}`);
 }
 
-Function.prototype.apply = function (obj = {}, args = []) {
+Function.prototype.apply = function (obj = {}, ...args) {
   if (typeof this !== "function") {
     throw new Error("Function is not callable");
   }
 
+  if (!Array.isArray(args)) {
+    throw new TypeError("CreateListFromArrayLike called on non-object");
+  }
+
   obj.fn = this;
-  obj.fn(args);
+  obj.fn(...args);
 };
 
 describeCity.apply(place, ["beautiful", "Elegant"]);
+// describeCity.apply(place, "beautiful");
