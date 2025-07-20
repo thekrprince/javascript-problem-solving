efde6a358636a82ffeb143dfd195ff87efc4c9a1
princekr96 <princekr700@gmail.com>
2025-07-20 10:26:10 +0530

    reduce polyfill

---
+++ JavaScript/Polyfills/3. Reduce.js
@@ -0,0 +1,14 @@
+// Polyfill of Reduce
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  let acc = initialValue;
+
+  for (let i = 0; i < this.length; i++) {
+    acc = acc ? callbackFn(acc, this[i]) : this[i];
+  }
+
+  return acc;
+};
+
+let arr = [1, 2, 3, 4, 5];
+const res = arr.myReduce((prev, curr) => prev + curr, 0);
+console.log(res);
