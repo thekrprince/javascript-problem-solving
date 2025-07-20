e249f7c7165876eae33f10440f3cea868f6bdbb2
princekr96 <princekr700@gmail.com>
2025-07-20 09:37:15 +0530

    map polyfill

--- JavaScript/Polyfills/1. Map.js
+++ JavaScript/Polyfills/1. Map.js
@@ -1,12 +1,29 @@
-Array.prototype.myMap = function (callback) {
-  console.log(callback);
-  var arr = [];
-  for (var i = 0; i < this.length; i++) {
-    arr.push(callback(this[i], i, this));
+// Polyfill based on GFE
+Array.prototype.myMap = function (callbackFn, thisArg) {
+  const len = this.length;
+  const res = new Array(len);
+
+  for (var i = 0; i < len; i++) {
+    if (i in this) {
+      res[i] = callbackFn.call(thisArg, this[i], i, this);
+    }
   }
-  return arr;
+  return res;
 };
 
 let myArr = [2, 3, 4];
 let res = myArr.myMap((data) => data + 1);
 console.log(res);
+
+// Simple Map Polyfill
+Array.prototype.simpleMap = function (callbackFn) {
+  const res = [];
+
+  for (var i = 0; i < this.length; i++) {
+    res.push(callbackFn(this[i], i, this));
+  }
+  return res;
+};
+
+let res2 = myArr.simpleMap((data) => data * 2);
+console.log(res2);
