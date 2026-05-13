b70909c9e3819f11d95d27224666231e7cc1aad9
thekrprince <princekr700@gmail.com>
2026-05-13 23:14:58 +0530

    throttle

--- GFE/4. throttle.js
+++ GFE/4. throttle.js
@@ -1,3 +1,31 @@
 // Throttling is a technique used to control how many times we allow a function to be executed over time. 
 // When a JavaScript function is said to be throttled with a wait time of X milliseconds, it can only be invoked at most once every X milliseconds. 
 // The callback is invoked immediately and cannot be invoked again for the rest of the wait duration.
+
+function throttle(func, wait) {
+    let shouldThrottle = false;
+
+    return function (...args) {
+        if (shouldThrottle) {
+            return;
+        }
+
+        shouldThrottle = true;
+        setTimeout(() => shouldThrottle = false, wait);
+
+        func.apply(this, args);
+    };
+}
+
+let i = 0;
+
+function increment() {
+    i++;
+    console.log("i is", i);
+}
+
+const throttledIncrement = throttle(increment, 2000);
+
+setInterval(() => {
+    throttledIncrement();
+}, 100);
